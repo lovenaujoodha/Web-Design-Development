a8d51b98456a7d5f43608b36739cf8f8d13fbcb7
lovenaujoodha <lovenaujoodha@gmail.com>
2023-06-19 19:58:24 +0400

    New project to push to Github

---
+++ db_connect..php
@@ -0,0 +1,7 @@
+<?php
+$host = "localhost";
+$username = "id4674653_agiletestdb";
+$password = "agileTestdb";
+$dbName = "id4674653_agiletest";
+
+$conn = mysqli_connect($host, $username, $password, $dbName);
